96c517e5479a16ca16ac1266d9e7ab66dec6d312
avertocle <avertocle@gmail.com>
2021-06-22 17:55:38 +0530

    vue and ts course stuff

---
+++ ud/vue/project02/src/components/friend_contact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsVisible ? "Hide Details" : "Show Details" }}
+    </button>
+    <ul v-if="detailsVisible">
+      <li><strong>Phone : </strong>{{ phoneNumber }}</li>
+      <li><strong>Email : </strong>{{ emailAdd }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props:[
+    'id',
+    'name',
+    'phoneNumber',
+    'emailAdd'
+  ],
+  data() {
+    return {
+      detailsVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsVisible = !this.detailsVisible;
+      console.log("details = " + this.detailsVisible);
+    },
+  },
+};
+</script>
